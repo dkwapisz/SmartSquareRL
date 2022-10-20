aa99ba6bfe3f8fa392a415ee573929dd1447d0fb
dkwapisz <dkwapisz99@gmail.com>
2022-10-20 23:13:40 +0200

    script fix

--- start_workers.sh
+++ start_workers.sh
@@ -1,9 +1,9 @@
-cd /home/dkwapisz/SmartSquareRL/cmake-build-release
+cd /home/dawidml/SmartSquareRL/cmake-build-release
 
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50051 &
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50052 &
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50053 &
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50054 &
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50055 &
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50056 &
-/home/dkwapisz/SmartSquareRL/cmake-build-release/SmartSquareRL 50057
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50051 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50052 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50053 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50054 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50055 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50056 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50057

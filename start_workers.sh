b4c6ecc3cfd2f02ada933bb75b18d25e6602a791
dkwapisz <dkwapisz99@gmail.com>
2022-10-21 00:29:08 +0200

    new script

--- start_workers.sh
+++ start_workers.sh
@@ -1,6 +1,5 @@
 cd /home/dawidml/SmartSquareRL/cmake-build-release
 
-Xvfb -ac :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
 /home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50051 &
 /home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50052 &
 /home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50053 &

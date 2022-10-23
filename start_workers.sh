2e1da00d5fd96b8d984e2fe38b93692bb3c97b28
dkwapisz <dkwapisz99@gmail.com>
2022-10-23 13:44:49 +0200

    parameters fix

--- start_workers.sh
+++ start_workers.sh
@@ -6,4 +6,5 @@ cd /home/dawidml/SmartSquareRL/cmake-build-release
 /home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50054 &
 /home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50055 &
 /home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50056 &
-/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50057
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50057 &
+/home/dawidml/SmartSquareRL/cmake-build-release/SmartSquareRL 50058
